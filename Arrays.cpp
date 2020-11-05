93adec1d3376fe89eecd1a67e8290af61998740b
Aman Kulshreshtha <aman21102000@gmail.com>
2020-11-05 20:33:12 +0530

    initial commit

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
-using namespace std;
+
+
+
+class Array {
+
+    private :
+        int *A;
+        int size;
+        int length;
+    public:
+
+        Array()
+        {
+            A = new int[20];             // just a random default size
+            size = 20;
+            length = 0;
+        }
+
+        Array(int size)
+        {
+            this->size = size ;
+            A = new int[size]; 
+            length = 0 ;
+
+
+        }
+
+        ~Array()
+        {
+
+            delete A;
+       
+        }
+
+        void set(int data,int position);
+        int get(int index);
+       void display();
+
+
+
+
+
+
+
+
+};
+
 
 
 
